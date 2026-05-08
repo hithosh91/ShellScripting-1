ec2dc1262594fbe62fc296b012533b44e97c2931
Hithosh Kota <kotahithosh@gmail.com>
2026-05-08 09:45:49 +0100

    Redirectors

--- 01_echo_topic.sh
+++ 01_echo_topic.sh
@@ -9,3 +9,20 @@ echo -e "\e[91mHello World\e[0m"
 #third command echo with background color
 echo -e "\e[44mHello World\e[0m"
 
+
+# redirecting output to a file
+#it contain three  redirecter standard input <, standard output >, and standard error 2>
+echo  "Devops"  # standard input redirected to standard output
+
+# redirecting standard output to a file
+
+echo "Devops" > file1.txt
+
+#redirecting standard error to a file
+
+dnf install nginx 2> error.log
+
+#save both standard output and standard error to a file
+
+dnf install nginx &> output.log
+
